06bc12ff2affc10ec6d07e92b226fbe2f609a4f1
zinchandrey <zinchandrey@gmail.com>
2020-01-21 19:58:06 +0300

    Добавляет появление / скрытие попапа с анимацией

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,28 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+// var pageHeader = document.querySelector('.page-header');
+// var headerToggle = document.querySelector('.page-header__toggle');
+//
+// pageHeader.classList.remove('page-header--nojs');
+//
+// headerToggle.addEventListener('click', function () {
+//   if (pageHeader.classList.contains('page-header--closed')) {
+//     pageHeader.classList.remove('page-header--closed');
+//     pageHeader.classList.add('page-header--opened');
+//   } else {
+//     pageHeader.classList.add('page-header--closed');
+//     pageHeader.classList.remove('page-header--opened');
+//   }
+// });
 
-pageHeader.classList.remove('page-header--nojs');
+var popup = document.getElementById('popup');
+var popupClose = document.getElementById('popup__close');
+var callback = document.getElementById('callback');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
+
+callback.addEventListener('click', function () {
+  popup.style.display = 'block';
+});
+
+popupClose.addEventListener('click', function () {
+  popup.style.display = 'none';
 });
