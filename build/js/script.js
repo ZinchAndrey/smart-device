bffd4b5e6529c707a528deaf71112005a6539ab3
zinchandrey <zinchandrey@gmail.com>
2020-01-22 22:00:49 +0300

    Добавляет кнопки скрыть / показать в подвале

--- build/js/script.js
+++ build/js/script.js
@@ -1,4 +1,55 @@
 'use strict';
+
+var popup = document.getElementById('popup');
+var popupClose = document.getElementById('popup__close');
+var callback = document.getElementById('callback');
+
+
+callback.addEventListener('click', function () {
+  if (popup.classList.contains('popup--closed')) {
+    popup.classList.remove('popup--closed');
+    popup.classList.add('popup--opened');
+  } else {
+    popup.classList.add('popup--closed');
+    popup.classList.remove('popup--opened');
+  }
+});
+
+popupClose.addEventListener('click', function () {
+  popup.classList.add('popup--closed');
+  popup.classList.remove('popup--opened');
+});
+
+// var navButton = document.getElementById('nav-button');
+// var footerNavList = document.getElementById('page-footer__nav-list');
+// var contactsButton = document.getElementById('contacts-button');
+// var contactsList = document.getElementById('page-footer__contacts-block');
+//
+// footerNavList.classList.remove('page-footer__nav-list--nojs');
+// contactsList.classList.remove('page-footer__contacts-block--nojs');
+//
+//
+// navButton.addEventListener('click', function () {
+//   if (footerNavList.classList.contains('page-footer__nav-list--closed')) {
+//     footerNavList.classList.remove('page-footer__nav-list--closed');
+//     footerNavList.classList.add('page-footer__nav-list--opened');
+//   } else {
+//     footerNavList.classList.add('page-footer__nav-list--closed');
+//     footerNavList.classList.remove('page-footer__nav-list--opened');
+//   }
+// });
+//
+// contactsButton.addEventListener('click', function () {
+//   if (contactsList.classList.contains('page-footer__contacts-block--closed')) {
+//     contactsList.classList.remove('page-footer__contacts-block--closed');
+//     contactsList.classList.add('page-footer__contacts-block--opened');
+//   } else {
+//     contactsList.classList.add('page-footer__contacts-block--closed');
+//     contactsList.classList.remove('page-footer__contacts-block--opened');
+//   }
+// });
+
+
 // var pageHeader = document.querySelector('.page-header');
 // var headerToggle = document.querySelector('.page-header__toggle');
 //
@@ -14,15 +65,15 @@
 //   }
 // });
 
-var popup = document.getElementById('popup');
-var popupClose = document.getElementById('popup__close');
-var callback = document.getElementById('callback');
-
-
-callback.addEventListener('click', function () {
-  popup.style.display = 'block';
-});
-
-popupClose.addEventListener('click', function () {
-  popup.style.display = 'none';
-});
+// var popup = document.getElementById('popup');
+// var popupClose = document.getElementById('popup__close');
+// var callback = document.getElementById('callback');
+//
+//
+// callback.addEventListener('click', function () {
+//   popup.style.display = 'block';
+// });
+//
+// popupClose.addEventListener('click', function () {
+//   popup.style.display = 'none';
+// });
